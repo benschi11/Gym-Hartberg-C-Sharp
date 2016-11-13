71b77fb51df3195bc8467e8356894672795c4434
Benedikt Neuhold <benedikt.neuhold@live.at>
2016-11-13 21:05:54 +0100

    Added Project "While-Schleife"

---
+++ 6. Klasse/While-Schleife/While-Schleife/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Benedikt Neuhold
+ * Date: 13.11.2016
+ * Time: 20:52
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace While_Schleife
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label m_lblOutput;
+		private System.Windows.Forms.Button m_btnCalc1;
+		private System.Windows.Forms.Button m_btnCalc2;
+		private System.Windows.Forms.Button m_btnCalc3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.m_lblOutput = new System.Windows.Forms.Label();
+			this.m_btnCalc1 = new System.Windows.Forms.Button();
+			this.m_btnCalc2 = new System.Windows.Forms.Button();
+			this.m_btnCalc3 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// m_lblOutput
+			// 
+			this.m_lblOutput.BackColor = System.Drawing.SystemColors.ControlLight;
+			this.m_lblOutput.ForeColor = System.Drawing.SystemColors.ControlText;
+			this.m_lblOutput.Location = new System.Drawing.Point(12, 9);
+			this.m_lblOutput.Name = "m_lblOutput";
+			this.m_lblOutput.Size = new System.Drawing.Size(318, 200);
+			this.m_lblOutput.TabIndex = 0;
+			// 
+			// m_btnCalc1
+			// 
+			this.m_btnCalc1.Location = new System.Drawing.Point(12, 225);
+			this.m_btnCalc1.Name = "m_btnCalc1";
+			this.m_btnCalc1.Size = new System.Drawing.Size(318, 46);
+			this.m_btnCalc1.TabIndex = 1;
+			this.m_btnCalc1.Text = "Berechne Quadratzahlen (händisch)";
+			this.m_btnCalc1.UseVisualStyleBackColor = true;
+			this.m_btnCalc1.Click += new System.EventHandler(this.M_btnCalcClick1);
+			// 
+			// m_btnCalc2
+			// 
+			this.m_btnCalc2.Location = new System.Drawing.Point(12, 289);
+			this.m_btnCalc2.Name = "m_btnCalc2";
+			this.m_btnCalc2.Size = new System.Drawing.Size(318, 46);
+			this.m_btnCalc2.TabIndex = 2;
+			this.m_btnCalc2.Text = "Berechne Quadratzahlen (While)";
+			this.m_btnCalc2.UseVisualStyleBackColor = true;
+			this.m_btnCalc2.Click += new System.EventHandler(this.M_btnCalc2Click);
+			// 
+			// m_btnCalc3
+			// 
+			this.m_btnCalc3.Location = new System.Drawing.Point(12, 352);
+			this.m_btnCalc3.Name = "m_btnCalc3";
+			this.m_btnCalc3.Size = new System.Drawing.Size(318, 46);
+			this.m_btnCalc3.TabIndex = 3;
+			this.m_btnCalc3.Text = "Berechne Quadratzahlen von geraden Zahlen";
+			this.m_btnCalc3.UseVisualStyleBackColor = true;
+			this.m_btnCalc3.Click += new System.EventHandler(this.M_btnCalc3Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(342, 479);
+			this.Controls.Add(this.m_btnCalc3);
+			this.Controls.Add(this.m_btnCalc2);
+			this.Controls.Add(this.m_btnCalc1);
+			this.Controls.Add(this.m_lblOutput);
+			this.Name = "MainForm";
+			this.Text = "While-Schleife";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ 6. Klasse/While-Schleife/While-Schleife/MainForm.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Benedikt Neuhold
+ * Date: 13.11.2016
+ * Time: 20:52
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace While_Schleife
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void M_btnCalcClick1(object sender, EventArgs e)
+		{
+			// Berechne Quadratzahlen von 1 bis 3
+			
+			// Methode 1 - Schreibe alle Händisch
+			int result1 = 1*1;
+			m_lblOutput.Text = Convert.ToString(result1);
+			
+			int result2 = 2*2;
+			m_lblOutput.Text = m_lblOutput.Text + Environment.NewLine + Convert.ToString(result2);
+			
+			int result3 = 3*3;
+			m_lblOutput.Text = m_lblOutput.Text + Environment.NewLine + Convert.ToString(result3);
+			
+			
+		}
+		void M_btnCalc2Click(object sender, EventArgs e)
+		{
+			// Zählvariable erstellen - wir starten mit 1*1 -> counter = 1
+			int counter = 1;
+			
+			// Solange die Variable counter kleiner oder gleich ist wie 3 führe aus
+			while(counter <=3)
+			{
+				int result = counter * counter;
+				m_lblOutput.Text = m_lblOutput.Text + Environment.NewLine + Convert.ToString(result);
+				counter++;
+			}
+		}
+		void M_btnCalc3Click(object sender, EventArgs e)
+		{
+			int counter = 2;
+			
+			while(counter <=100)
+			{
+				int result = counter * counter;
+				
+				// Vermeide - zu Beginn
+				if(counter == 2)
+				{
+					m_lblOutput.Text = Convert.ToString(result);
+				}
+				else
+				{
+					m_lblOutput.Text = m_lblOutput.Text + " - " + Convert.ToString(result);
+				}
+
+				counter = counter +2;
+			}
+		}
+	}
+}
